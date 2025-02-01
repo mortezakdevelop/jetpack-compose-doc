b90aef9316786a423dd973c26853c147b7145f40
M.Kalateh <morteza.kalaateh2105@gmail.com>
2025-02-01 18:46:14 +0330

    feat: learn alignment in compose

--- app/src/main/java/com/example/jetpackcomposepracticedocument/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepracticedocument/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.jetpackcomposepracticedocument
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepracticedocument.ui.theme.JetpackComposePracticeDocumentTheme
@@ -30,12 +35,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposePracticeDocumentTheme {
-                Surface {
-                    ShowMessage(MessageList("Android", "Compose"))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                content = {
+                    // Here, SimpleText is a Composable function which is going to describe the contents of
+                    // this activity that will be rendered on the screen.
+                    ShowTextOnCenterScreen("This is the Learn Jetpack Compose By Example tutorial")
                 }
-            }
+            )
         }
+
     }
 }
 
@@ -52,21 +63,36 @@ fun ShowMessage(name: MessageList) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
         Spacer(modifier = Modifier.size(8.dp))
         Column {
-            Text(text = name.name)
+            Text(text = name.name, color = MaterialTheme.colorScheme.secondary)
+            Spacer(modifier = Modifier.height(12.dp))
             Text(text = name.family)
         }
     }
 }
 
+@Composable
+fun ClickCounter(click: Int, onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text("Clicked on text $click time")
+    }
+}
+
+//show a simple text on center screen
+@Composable
+fun ShowTextOnCenterScreen(text: String) {
+    Text(text = text)
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun ShowMessagePreview() {
     JetpackComposePracticeDocumentTheme {
-        Surface {
-            ShowMessage(MessageList("Android", "Compose"))
-        }
+        ShowMessage(MessageList("Android", "Compose"))
+
     }
 }
